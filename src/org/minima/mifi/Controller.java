9e328ccc42c5425a1b6fe21ab9ae78c2f1d0792a
spartacusrex <patrickcerri@gmail.com>
2020-02-10 11:21:18 +0000

    Startup..

---
+++ src/org/minima/mifi/Controller.java
@@ -0,0 +1,18 @@
+package org.minima.mifi;
+
+import org.utils.messages.Message;
+import org.utils.messages.MessageProcessor;
+
+public class Controller extends MessageProcessor {
+
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void processMessage(Message zMessage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/org/minima/mifi/webproxy.java
@@ -0,0 +1,13 @@
+package org.minima.mifi;
+
+public class webproxy {
+
+	public static void main(String[] zArgs) {
+		
+		System.out.println("Starting MiFi Web proxy..");
+		
+		
+		
+	}
+	
+}
